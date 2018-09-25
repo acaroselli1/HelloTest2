571960fba6a81b19d648f182f53a8018443bb534
Alexander Caroselli <Alexander.caroselli@allata.com>
2018-09-24 19:33:17 -0600

    Add project files.

---
+++ HelloTest2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloTest2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
