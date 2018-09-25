b5c21f070b038ceefcb2ae2e55ba7906932a67d5
Alexander Caroselli <Alexander.caroselli@allata.com>
2018-09-24 20:41:54 -0600

    guess a number

--- HelloTest2/Program.cs
+++ HelloTest2/Program.cs
@@ -6,7 +6,45 @@ namespace HelloTest2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var random = new Random();
+            int answer = random.Next(1, 50);
+            int guesses = 0;
+            Console.WriteLine("Try guessing my number.");
+
+            bool playing = true;
+
+            while (playing) {
+
+            var guess = Console.ReadLine();
+            guesses++;
+
+            int.TryParse(guess, out int x );
+
+            if (x == answer)
+            {
+
+                Console.WriteLine("great guess");
+                playing = false;
+                break;
+            }
+
+                    
+            if (x > answer)
+            {
+               Console.WriteLine("Guess Lower");
+               continue;
+                    
+            }
+            if (x < answer)
+            {
+             Console.WriteLine("Guess Higher");
+             continue;
+
+            }
+            }
+
+            Console.WriteLine("You Guessed it!");
+            Console.ReadLine();
         }
     }
 }
